352e12851c1474c9ddca33b383525bde7118b7ec
Phantom0118 <suwimol586v@gmail.com>
2025-08-22 10:27:11 +0700

    v.4

---
+++ test06.py
@@ -0,0 +1,29 @@
+# operator ตัวดำเนินการ คือ เครื่องหมายที่ใช้ในการประมวลผลข้อมูล
+# 1. arithmetic operator เครื่องหมายคำนวณ
+# (+,-,*,/, % mod , // foor , ** eponential)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3) #หารเอาเศษ
+print(10 // 3) #หารเอาส่วน
+print(10 ** 3) 
+print('++++++++++++++++++++++++++++++++')
+# 2. comperison operator เครื่องหมายเปรียบเทียบ (==, !=, >, <, >=, <=)
+print(10 == 3)
+print(10 != 3)
+print('Bangna' >= 'Bangpoo')
+print('Thailand' < 'THAILAND')
+# 3. logical operator เครื่องหมาย ตรรกะ not,and,or
+print(not True)
+print(not False)
+print('--------')
+print(True and True)#ture
+print(False and False)
+print(False and True)
+print(True and False)
+print('--------')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)#false
